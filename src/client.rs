ba4757ccbeb26a9f71615ecece2b6f1749fb699a
Sealmex <semueljuliotangkuman21@gmail.com>
2022-04-16 16:56:16 +0800

    Remove Threading Code

--- src/client.rs
+++ src/client.rs
@@ -1,43 +1,14 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
-use std::thread;
-use std::time::Duration;
 fn main() {
 
     // Simple TCP Client
-    // if let Ok(mut stream) = TcpStream::connect("127.0.0.1:8000") {
-    //     let msg = "Hello!";
-    //     println!("Connected to the server!");
-    //     println!("Sending a message.");
-    //     stream.write(msg.as_bytes()).expect("Failed to write message back.");
-    // } else {
-    //     println!("Couldn't connect to server...");
-    // }
-
-    // Test
-    thread::spawn(|| {
-        for i in 1..5 {
-            if let Ok(mut stream) = TcpStream::connect("127.0.0.1:8000") {
-                let msg = "Hello!";
-                println!("Connected to the server!");
-                println!("Sending a message.");
-                stream.write(msg.as_bytes()).expect("Failed to write message back.");
-            } else {
-                println!("Couldn't connect to server...");
-            }
-            thread::sleep(Duration::from_millis(1));
-        }
-    });
-
-    for i in 1..5 {
-        if let Ok(mut stream) = TcpStream::connect("127.0.0.1:8000") {
-            let msg = "Im the Client!";
-            println!("Connected to the server!");
-            println!("Sending a message.");
-            stream.write(msg.as_bytes()).expect("Failed to write message back.");
-        } else {
-            println!("Couldn't connect to server...");
-        }
-        thread::sleep(Duration::from_millis(1));
+    if let Ok(mut stream) = TcpStream::connect("127.0.0.1:8000") {
+        let msg = "Hello!";
+        println!("Connected to the server!");
+        println!("Sending a message.");
+        stream.write(msg.as_bytes()).expect("Failed to write message back.");
+    } else {
+        println!("Couldn't connect to server...");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,17 @@
 use std::io::prelude::*;
 use std::str::from_utf8;
 use std::net::{TcpListener, TcpStream};
-use std::thread;
+
 fn main() {
     let listen = TcpListener::bind("127.0.0.1:8000").unwrap();
     println!("Waiting for connections...");
-    let mut id = 0;
     for stream in listen.incoming() {
-        id += 1;
-        thread::spawn(move || 
-        {
-            let stream = stream.unwrap();
-            handle_stream(stream, id);
-        });
+        let stream = stream.unwrap();
+        handle_stream(stream);
     }
 }
 
-fn handle_stream(mut stream: TcpStream, id: u8) {
-    println!("Thread ID is {}", id);
+fn handle_stream(mut stream: TcpStream) {
     let mut body = Vec::new();
     println!("Connection establised with {}", stream.peer_addr().unwrap());
     stream.read_to_end(&mut body).unwrap();
