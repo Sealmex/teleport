e7a5d66e65e85cb658c0624a81251649c3cc1d53
Sealmex <semueljuliotangkuman21@gmail.com>
2022-04-17 17:33:52 +0800

    get the length of head

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::io::prelude::*;
-use std::str::from_utf8;
+use std::str::{from_utf8};
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
@@ -13,7 +13,7 @@ fn main() {
 }
 // handle streams
 fn handle_transfer(mut stream: TcpStream) {
-    let mut data = Vec::new();
+    let mut data = Vec::<u8>::new();
     println!("Connection establised with {}", stream.peer_addr().unwrap());
     match stream.read_to_end(&mut data) {
         Ok(_) => println!("Read data."),
@@ -22,12 +22,18 @@ fn handle_transfer(mut stream: TcpStream) {
     println!("Data is {}", from_utf8(&data).unwrap());
     // debugging
     println!("Raw data {:?}",&data);
+    let mut head = Vec::new();
     for id in data {
+        head.push(id);
         if id == 226 {
             println!("Found it! {}", id);
+            let head_length = from_utf8(&head).unwrap_err();
+            println!("Data length before 226 is {:?}", head_length.valid_up_to());
             break;
         } else {
             println!("Doesnt seem to exist")
         }
     }
 }
+
+
