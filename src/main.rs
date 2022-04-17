959588d94b51ef3e5b5f0f93edfd1490ff1b9393
Sealmex <semueljuliotangkuman21@gmail.com>
2022-04-17 17:02:15 +0800

    test to find 226 (U-2800)

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::str::from_utf8;
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
+    // listener
     let listen = TcpListener::bind("127.0.0.1:8000").unwrap();
     println!("Waiting for connections...");
     for stream in listen.incoming() {
@@ -10,7 +11,7 @@ fn main() {
         handle_transfer(stream);
     }
 }
-
+// handle streams
 fn handle_transfer(mut stream: TcpStream) {
     let mut data = Vec::new();
     println!("Connection establised with {}", stream.peer_addr().unwrap());
@@ -18,5 +19,15 @@ fn handle_transfer(mut stream: TcpStream) {
         Ok(_) => println!("Read data."),
         Err(_) => println!("Failed to read Data.")
     }
-    println!("Data is {}", from_utf8(&data).unwrap())
+    println!("Data is {}", from_utf8(&data).unwrap());
+    // debugging
+    println!("Raw data {:?}",&data);
+    for id in data {
+        if id == 226 {
+            println!("Found it! {}", id);
+            break;
+        } else {
+            println!("Doesnt seem to exist")
+        }
+    }
 }
