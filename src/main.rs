2f8a8f427efccf9910a5e7ae64b9219236e45e8c
Sealmex <semueljuliotangkuman21@gmail.com>
2022-05-03 09:56:20 +0800

    code optimization

--- src/main.rs
+++ src/main.rs
@@ -22,24 +22,17 @@ fn handle_transfer(mut stream: TcpStream) {
     println!("Data is {}", from_utf8(&data).unwrap());
     // debugging
     println!("Raw data {:?}",&data);
-    handle_data(data)
+    handle_data(data);
 }
 
-fn handle_data(data: Vec::<u8>) {
-    let mut head = Vec::new();
-    for id in data {
-        head.push(id);
-        if id == 226 {
-            println!("Found it! {}", id);
-            let head_length = from_utf8(&head).unwrap_err().valid_up_to();
-            println!("Data length before 226 is {}", head_length);
-            head.pop();
-            println!("Data before 226 is {}", from_utf8(&head).unwrap());
-            break;
-        } else {
-            println!("Doesnt seem to exist")
-        } 
+fn handle_data(mut data_received: Vec::<u8>) {
+    let mut data = data_received;
+    fn save_data(filename: &str, filedata: Vec::<u8>){
+
     }
+    let index = data.iter().position(|&x|x == 226).unwrap();
+    println!("Index is {}", index);
+    println!("Byte in index is {}", data[index])
 }
 
 
