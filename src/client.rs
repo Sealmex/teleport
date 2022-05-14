9adf5410b7b3ad483b206a90f3df8bf82212a952
Sealmex <semueljuliotangkuman21@gmail.com>
2022-05-14 20:49:22 +0800

    Small changes

--- src/client.rs
+++ src/client.rs
@@ -1,11 +1,10 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
-
+use std::env::args;
 fn main() {
-
     // Simple TCP Client
     if let Ok(mut stream) = TcpStream::connect("127.0.0.1:8000") {
-        let data = "test.txt".to_owned() + "\0" + "pretend this is an image";
+        let data = "modular.txt".to_owned() + "\0" + "modular rework test";
         println!("Connected to the server!");
         println!("Sending data.");
         stream.write_all(data.as_bytes());
