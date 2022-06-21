33ae20b9a8b65a7328b1afdd01c70fa7e95cc788
Sealmex <semueljuliotangkuman21@gmail.com>
2022-06-21 17:42:50 +0800

    validate and debug_print function added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+#![feature(fs_try_exists)]
+// Cuz am lazy
+use std::{env, fs, path::{Path}};
 fn main() {
-    teleport::send();
+    // Confusing Naming Conventions.
+    let arguments = env::args();
+    let mut args = Vec::new();
+    for argument in arguments {args.push(argument)};
+    if args[1] == "send" {
+        if validate(args[2].as_str()) {debug_print("Pass")} else {eprintln!("Error: Did not Pass!")}
+    } else if args[1] == "receive" {
+        teleport::receive()
+    } else if args.is_empty() {
+        eprintln!("Error: No Supplied Arguments!")
+    }
+}
+/// ### Validate Filenames
+/// - Does file already Exist \
+/// - Does text provided actually have an extension
+fn validate(filename: &str) -> bool {
+    let exist_already: bool = fs::try_exists(filename).unwrap();
+    let extension_exist: bool= Path::new(filename).extension().is_some();
+    // Checks the return value
+    if extension_exist == true {if exist_already == false {debug_print("Passed all Checks!"); true} else {debug_print("Failed check 2"); false}} else {debug_print("Failed check 1"); false}
+}
+/// Debug Print
+/// Only print if Debug Variable is set
+pub fn debug_print(text: &str) {
+    // Check if debug assertion flag is set and print the given text
+    if cfg!(debug_assertions) {
+        println!("{}", text);
+    } else {} // Do nothing
 }
