dfda5086ecaf37553c89624344f832de1cf5fd21
Sealmex <semueljuliotangkuman21@gmail.com>
2022-06-22 21:16:20 +0800

    Refactor: Moved special_print from main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,52 @@
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
-/* Not needed for the current moment
+use std::time::Duration;
+use std::env;
 use serde::{Serialize, Deserialize};
 #[derive(Serialize, Deserialize)]
-struct Data {
-    filename: String,
-    filesize: u64,
-    filedata: Vec<u8>
+/// This Struct **SHOULD** Contain All Basic Metadata Informations From a File.
+struct FileInfo {
+    name: String,
+    size: u64,
+    data: Vec<u8>,
+    last_modified: u32,
+    last_accessed: u32,
+    creation_time: u32,
 }
-*/
-// Function to start listening for connections, to send files.
-pub fn send() {
-    println!("Starting...");
+/// Function to start listening for connections, to send files. Takes a filename of type string
+pub fn send(filename: &str) {
+    special_print("Starting...");
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
-    for stream in listener.incoming() {
+    for stream_listener in listener.incoming() {
+        let stream = stream_listener.unwrap();
         println!("Connection Established!");
-        handle_connections(stream.unwrap());
+        special_print(format!("Connected to {:?}", stream.peer_addr().unwrap()).as_str()); // 3 Methods in a row :v
+        handle_connections_send(stream, filename.to_owned());
     }
-    fn handle_connections(stream: TcpStream) {
-    }
+}
+
+/// Function that will start to connect to certain addresses, to receive files.
+pub fn receive() {
+    println!("Starting...");
+    let addr = "127.0.0.1:8080".parse().unwrap();
+    let stream_connect = TcpStream::connect_timeout(&addr, Duration::from_secs(10));
+    println!("Connected.");
+    handle_connection_receive(stream_connect.unwrap())
+}
+
+fn handle_connections_send(stream: TcpStream, filename: String) {
+    println!("Sending {}", filename)
+}
+
+fn handle_connection_receive(stream: TcpStream) {
+
+}
+
+/// Special Function to only print if a env_variable set early in main.rs is set to enabled
+pub fn special_print(text: &str) {
+    // Check if var "vb" is set to enabled and print the given text
+    if env::var("vb").unwrap() == "enabled" {
+        println!("{}", text);
+    } else {
+    } // Do nothing
 }
