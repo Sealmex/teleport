a9dbd15d0bbfd5a70b242e50e0555feaca6110a7
Sealmex <semueljuliotangkuman21@gmail.com>
2022-06-29 22:05:54 +0800

    Update: Fully working Prototype, Halfway done!
    This version of lib.rs is able to send/receive a file and save it.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-use std::{{self, io::prelude::*}, env, fs::{self, File}, time::SystemTime};
+use std::{{io::prelude::*}, env, fs, time::SystemTime, net::SocketAddr, str::from_utf8};
 use std::net::{TcpListener, TcpStream};
-use std::time::Duration;
 use serde::{Serialize, Deserialize};
+use fs_set_times::{set_atime, set_mtime, SystemTimeSpec};
 #[derive(Serialize, Deserialize, Debug)]
 /// This Struct **SHOULD** Contain All Basic Metadata Informations From a File.
 struct FileInfo {
@@ -9,7 +9,6 @@ struct FileInfo {
     data: Vec<u8>,
     last_modified: SystemTime,
     last_accessed: SystemTime,
-    creation_time: SystemTime,
 }
 
 impl FileInfo {
@@ -18,14 +17,13 @@ impl FileInfo {
             size: fs::metadata(file_path).unwrap().len(),
             last_modified: fs::metadata(file_path).unwrap().modified().unwrap(),
             last_accessed: fs::metadata(file_path).unwrap().accessed().unwrap(),
-            creation_time: fs::metadata(file_path).unwrap().created().unwrap(),
             data: fs::read(file_path).unwrap(),
         }
     }
 }
 /// Function to start listening for connections, to send files. Takes a filename of type string
 pub fn send(file_path: &str) {
-    println!("Info: Connecting with 127.0.0.1:8080"); // Temp
+    println!("Info: Connecting to 127.0.0.1:8080"); // Temp
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     for stream_listener in listener.incoming() {
         let stream = stream_listener.unwrap();
@@ -38,25 +36,42 @@ pub fn send(file_path: &str) {
 /// Function that will start to connect to certain addresses, to receive files.
 pub fn receive(file_path: &str) {
     println!("Info: Connecting to 127.0.0.1:8080"); // Temp
-    let addr = "127.0.0.1:8080".parse().unwrap();
-    let stream_connect = TcpStream::connect_timeout(&addr, Duration::from_secs(10));
-    let stream = stream_connect.unwrap();
+    let addr: SocketAddr = "127.0.0.1:8080".parse().unwrap();
+    let stream_connect = TcpStream::connect(addr).expect("Error: Failed to Connect to Client!");
+    let stream = stream_connect;
     println!("Info: Connected.");
     special_print(format!("Connected to {:?}", stream.peer_addr().unwrap()).as_str());
-    handle_connection_receive(stream)
+    handle_connection_receive(stream, file_path)
 }
 
-fn handle_connections_send(stream: TcpStream, file_path: String) {
+fn handle_connections_send(mut stream: TcpStream, file_path: String) {
     special_print("Initializing data struct.");
-    let file: FileInfo = FileInfo::new(&file_path);
-    special_print(format!("{:?}", file).as_str());
+    let file = FileInfo::new(&file_path);
+    // special_print(format!("{:?}", file).as_str());
+    special_print(format!("Sending File to {:?}", stream.peer_addr().unwrap()).as_str());
+    let serialized_data = serde_json::to_string(&file).unwrap();
+    stream.write_all(serialized_data.as_bytes()).expect("Error: Failed to write data to stream!");
 }
 
-fn handle_connection_receive(stream: TcpStream) {
-
+fn handle_connection_receive(mut stream: TcpStream,file_path: &str) {
+    fn write_to_file(file_path: &str, file_struct: FileInfo) {
+        let mut file = fs::File::create(file_path).unwrap();
+        file.write_all(&file_struct.data).unwrap();
+        set_atime(file_path, SystemTimeSpec::Absolute(file_struct.last_accessed)).unwrap();
+        set_mtime(file_path, SystemTimeSpec::Absolute(file_struct.last_modified)).unwrap();
+        println!("Successfully saved file!");
+    }
+    let mut buffer = String::new();
+    let received_data = stream.read_to_string(&mut buffer);
+    special_print(format!("Bytes Read: {:?}", received_data.unwrap()).as_str());
+    let file: FileInfo = serde_json::from_str(&buffer).unwrap();
+    special_print(format!("Data Received: {:?}", from_utf8(&file.data).unwrap()).as_str());
+    special_print(format!("Writing file to filepath: {:?}", file_path).as_str());
+    write_to_file(file_path, file)
 }
 
 /// Println! macro wrapped with a simple check for a env variable to actually do stuff
+/// TODO -> Implement format arguments instead of using format! every time!
 pub fn special_print(text: &str) {
     // Check if var "vb" is set to enabled and print the given text
     if env::var("vb").unwrap() == "enabled" {
